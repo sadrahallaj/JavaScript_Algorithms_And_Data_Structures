06b44ecd295aa6362fc710b63fbdf3389882a373
emadbaqeri <embaqeri1998@gmail.com>
2021-03-11 14:47:24 -0500

    average from items of an array

--- day01/01-weekly-weather.js
+++ day01/01-weekly-weather.js
@@ -8,10 +8,19 @@ const week_days = [
   "Saturday",
 ];
 
-const weekly_weather = [70, 72, 69, 67, 70, 71, , 74];
+const weekly_weather = [70, 72, 69, 67, 70, 71, 74];
 
 for (let i = 0; i < week_days.length; i++) {
   let weekday = week_days[i];
   let temprature = weekly_weather[i];
   console.log(`temprature on ${weekday} was ${temprature} degrees`);
 }
+
+let total_temp = 0;
+for (let i = 0; i < weekly_weather.length; i++) {
+  let temp = weekly_weather[i];
+  total_temp += temp;
+}
+
+let temp_average_weekly = Math.round(total_temp / weekly_weather.length);
+console.log(temp_average_weekly);
